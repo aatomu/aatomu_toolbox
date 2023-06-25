db378d78bc29de6420fa81a4c9723904d0c44f9f
atomu21263 <atom.aizu@gmail.com>
2023-06-25 22:40:17 +0900

    Amazonのやつを変数化

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,23 @@
-chrome.storage.sync.get(["isOpenAmazonMode"]).then((result) => {
+let user = "Atomu"
+let address = "Minecraft"
+let postCode = "The_Overworld"
+let greeting = "/say"
+
+chrome.storage.sync.get(["isOpenAmazonMode"]).then(async (result) => {
   if (!result.isOpenAmazonMode) {
-    const amazonCss = chrome.runtime.getURL('css/amazon.css')
+    // 仮CSS
+    document.querySelector(`head`).innerHTML += `<link id="secretDummy" rel="stylesheet" href="${chrome.runtime.getURL('css/amazon.css')}">`
+    // 置換準備
+    let amazonCss = await fetch(chrome.runtime.getURL('css/amazon.css'))
+      .then(r => { return r.text() })
+    // 置換
+    amazonCss = amazonCss.replaceAll('${User}',user)
+    amazonCss = amazonCss.replaceAll('${Address}',address)
+    amazonCss = amazonCss.replaceAll('${PostCode}',postCode)
+    amazonCss = amazonCss.replaceAll('${Greeting}',greeting)
     console.log(amazonCss)
-    document.querySelector(`head`).innerHTML += `<link rel="stylesheet" href="${chrome.runtime.getURL('css/amazon.css')}">`
+    // 設置
+    document.querySelector(`head`).insertAdjacentHTML("beforeend",`<style>${amazonCss}</style`)
+    document.getElementById("secretDummy").remove()
   }
 })
