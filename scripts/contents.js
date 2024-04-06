dd053e27200c6faa63e2a5e0f69d86f4faa60986
atomu21263 <atom.aizu@gmail.com>
2024-04-07 00:33:55 +0900

    コードをきれいに

--- scripts/contents.js
+++ scripts/contents.js
@@ -1,40 +1,28 @@
-// Global
-//// 文字列を変換
 function str2HTML(html) {
   const dummyElement = document.createElement('div');
   dummyElement.innerHTML = html;
   return dummyElement.firstElementChild;
 }
 
-//// いろんなサイトのクリーパー
-if (/https?/.test(location.protocol)) {
+// Protocol Check
+const HTML_Body = document.body
+console.log(HTML_Body)
+if (HTML_Body) {
+  // Creeper
   chrome.storage.sync.get(["isViewCreeper"]).then(result => {
     if (result.isViewCreeper) {
-      const el = str2HTML(`<img class="creeper" id="creeper" src="${chrome.runtime.getURL("images/creeper.webp")}">`)
-      document.body.appendChild(el)
-      document.getElementById("creeper").addEventListener("mouseover", function (e) {
-        document.getElementById("creeper").classList.add("creeper_fade")
+      const creeper = str2HTML(`<img class="creeper" id="creeper" src="${chrome.runtime.getURL("images/creeper.webp")}">`)
+      HTML_Body.appendChild(creeper)
+      creeper.addEventListener("mouseover", function () {
+        creeper.classList.add("creeper_fade")
       })
     }
   })
 
+  const insertScript = document.createElement('script');
+  insertScript.src = chrome.runtime.getURL('scripts/contents_insert.js');
+  insertScript.onload = function () {
+    this.remove();
+  };
+  HTML_Body.appendChild(insertScript)
 }
-
-//// 変数チェック
-let s = document.createElement('script');
-s.src = chrome.runtime.getURL('scripts/contents_insert.js');
-s.onload = function () {
-  this.remove();
-};
-document.getElementsByTagName("body")[0].appendChild(s);
-
-
-// document.querySelectorAll("*").forEach(el => {
-//   for (node of el.childNodes) {
-//     if (node.nodeType == Node.TEXT_NODE && !"SCRIPT,STYLE".includes(node.parentNode.nodeName)) {
-//       node.nodeValue = node.nodeValue.replace("ない", "無い")
-//       node.nodeValue = node.nodeValue.replace(/[ぁ-んァ-ヴｦ-ﾟ]/g, "")
-//       console.log(node)
-//     }
-//   }
-// })
