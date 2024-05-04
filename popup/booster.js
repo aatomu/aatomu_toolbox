f4ba095fac4f162a7df076bba0714e6630028e37
atomu21263 <atom.aizu@gmail.com>
2024-05-05 01:04:50 +0900

    音量を表示

--- popup/booster.js
+++ popup/booster.js
@@ -1,6 +1,12 @@
-const audioCtx = new AudioContext()
+// Audio Context
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const gainNode = audioCtx.createGain()
+const analyzerNode = audioCtx.createAnalyser()
+analyzerNode.fftSize = 1024
+const analyzeBinLength = analyzerNode.frequencyBinCount
+const analyzeBin = new Uint8Array(analyzeBinLength)
 
+// Audio information(by URL)
 const url = new URL(window.location.href)
 const searchParams = new URLSearchParams(url.searchParams)
 const tabID = parseInt(searchParams.get("id"))
@@ -9,8 +15,11 @@ document.getElementById("inline_title").innerText = `${searchParams.get("title")
 
 let isBoosted = false
 
+// HTML element
 const amplifier = document.getElementById("amplifier")
 const amplifierValue = document.getElementById("amplifierValue")
+const volumeArea = document.getElementById("volumeArea")
+const volumeLine = document.getElementById("volumeLine")
 
 amplifier.addEventListener("input", async function () {
   if (!isBoosted) {
@@ -25,10 +34,28 @@ amplifier.addEventListener("input", async function () {
       },
       video: false
     })
+
     const track = audioCtx.createMediaStreamSource(media)
-    track.connect(gainNode).connect(audioCtx.destination)
+    track.connect(gainNode).connect(analyzerNode).connect(audioCtx.destination)
     isBoosted = true
+
+    setInterval(analyze,10)
   }
   gainNode.gain.value = amplifier.value
   amplifierValue.innerText = `x${amplifier.value}`
 })
+
+function analyze() {
+  analyzerNode.getByteFrequencyData(analyzeBin)
+
+  const areaHeight = volumeArea.clientHeight
+  const stepHeight = areaHeight / 255
+  const areaWidth = volumeArea.clientWidth
+  const stepWidth = areaWidth / analyzeBinLength
+
+  let line = `M0,${areaHeight - analyzeBin[0] * stepHeight} `
+  for (let i = 1; i < analyzeBinLength; i++) {
+    line += `L${i * stepWidth},${areaHeight - analyzeBin[i] * stepHeight} `
+  }
+  volumeLine.setAttribute("d",line)
+}
