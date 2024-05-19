3ed5ddda9f519ee6f2e2ec9a8634d87b90d1a399
atomu21263 <atom.aizu@gmail.com>
2024-05-19 21:41:33 +0900

    サイズをいい感じに

--- scripts/background.js
+++ scripts/background.js
@@ -105,7 +105,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
         type: "popup",
         url: chrome.runtime.getURL("popup/booster.html") + `?id=${tab.id}`,
         width: 800,
-        height: 500
+        height: 420
       })
     case "short2movie":
       // shortのページ以外で実行しない
