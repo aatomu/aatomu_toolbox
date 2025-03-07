5c6cdd0725a097cbe7f0212391ba0087df3ac015
atomu21263 <aatomu21263+git@gmail.com>
2025-03-07 22:56:54 +0900

    🐛 元情報が出てしまうのを修正

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,31 @@
-// 仮CSS
-document.querySelector(`head`).insertAdjacentHTML("beforeend", `<link id="secretDummy" rel="stylesheet" href="${chrome.runtime.getURL('css/amazon.css')}">`)
-// CSS読み込み
-chrome.storage.sync.get(["Setting"]).then(async (result) => {
+document.addEventListener("DOMContentLoaded", async () => {
+  // CSS読み込み
+  const storage = await chrome.storage.sync.get(["Setting"]).then(result => { return result })
+
   // secretモードか
-  if (!result.Setting.SecretAmazonMode) {
+  if (!storage.Setting.SecretAmazonMode) {
     return
   }
-  // 置換準備
-  let amazonCss = await fetch(chrome.runtime.getURL('css/amazon.css'))
-    .then(r => { return r.text() })
+
   // 置換
-  amazonCss = amazonCss.replaceAll('${User}', result.Setting.User)
-  amazonCss = amazonCss.replaceAll('${Address}', result.Setting.Address)
-  amazonCss = amazonCss.replaceAll('${PostCode}', result.Setting.PostCode)
-  amazonCss = amazonCss.replaceAll('${Greeting}', result.Setting.Greeting)
-  console.log(amazonCss)
+  let amazonCSS = await fetch(chrome.runtime.getURL('css/amazon.css')).then(r => { return r.text() })
+  amazonCSS = amazonCSS.replaceAll('${User}', storage.Setting.User)
+  amazonCSS = amazonCSS.replaceAll('${Address}', storage.Setting.Address)
+  amazonCSS = amazonCSS.replaceAll('${PostCode}', storage.Setting.PostCode)
+  amazonCSS = amazonCSS.replaceAll('${Greeting}', storage.Setting.Greeting)
+  console.log(amazonCSS)
+
   // 設置
-  document.querySelector(`head`).insertAdjacentHTML("beforeend", `<style>${amazonCss}</style`)
+  document.querySelector(`head`).insertAdjacentHTML("beforeend", `<style>${amazonCSS}</style>`)
 
   // 購入ボタンの表示
-  if (result.Setting.ShowAmazonBuyButton) {
+  if (storage.Setting.ShowAmazonBuyButton) {
     let buttons = document.querySelectorAll("#submit\\.buy-now,#sc-buy-box-ptc-button")
-    for (i=0;i<buttons.length;i++) {
+    for (i = 0; i < buttons.length; i++) {
       buttons[i].classList.add("amazon-unsafety")
     }
   }
-})
-// 仮CSS削除
-document.getElementById("secretDummy").remove()
-// 非表示解除
-setTimeout(function () {
+
+  // 非表示解除
   document.body.classList.add("isSecretLoaded")
 }, 500)
--- scripts/google.js
+++ scripts/google.js
@@ -1,20 +1,16 @@
-// 仮CSS
-document.querySelector(`head`).insertAdjacentHTML("beforeend",`<link id="secretDummy" rel="stylesheet" href="${chrome.runtime.getURL('css/google.css')}">`)
-// 読み込み
-chrome.storage.sync.get(["Setting"]).then(async (result) => {
-  // 置換準備
-  let googleCss = await fetch(chrome.runtime.getURL('css/google.css'))
-    .then(r => { return r.text() })
+document.addEventListener("DOMContentLoaded", async () => {
+  // 読み込み
+  const storage = await chrome.storage.sync.get(["Setting"]).then(result => { return result })
+
   // 置換
-  googleCss = googleCss.replaceAll('${Address}', result.Setting.Address)
-  googleCss = googleCss.replaceAll('${PostCode}', result.Setting.PostCode)
-  console.log(googleCss)
+  let googleCSS = await fetch(chrome.runtime.getURL('css/google.css')).then(r => { return r.text() })
+  googleCSS = googleCSS.replaceAll('${Address}', storage.Setting.Address)
+  googleCSS = googleCSS.replaceAll('${PostCode}', storage.Setting.PostCode)
+  console.log(googleCSS)
+
   // 設置
-  document.querySelector(`head`).insertAdjacentHTML("beforeend", `<style>${googleCss}</style`)
-})
-// 仮CSS 削除
-document.getElementById("secretDummy").remove()
-// 非表示解除
-setTimeout(function() {
+  document.querySelector(`head`).insertAdjacentHTML("beforeend", `<style>${googleCSS}</style>`)
+
+  // 非表示解除
   document.body.classList.add("isSecretLoaded")
-},500)
+})
