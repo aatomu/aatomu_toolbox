91cad6574cdb5fb53e9d2fe0946ca92b55a5749f
atomu21263 <aatomu21263+git@gmail.com>
2025-05-05 17:05:11 +0900

    ⚡️ creeperの追加を丁寧に

--- scripts/contents.js
+++ scripts/contents.js
@@ -1,17 +1,14 @@
 console.log("contents.js loaded")
-function str2HTML(html) {
-  const dummyElement = document.createElement('div');
-  dummyElement.innerHTML = html;
-  return dummyElement.firstElementChild;
-}
-
 // Protocol Check
 const HTML_Body = document.body
 if (HTML_Body) {
   // Creeper
   chrome.storage.sync.get(["isViewCreeper"]).then(result => {
     if (result.isViewCreeper) {
-      const creeper = str2HTML(`<img class="creeper" id="creeper" src="${chrome.runtime.getURL("images/creeper.webp")}">`)
+      const creeper = document.createElement('img');
+      creeper.id="creeper"
+      creeper.classList.add("creeper")
+      creeper.src= chrome.runtime.getURL("images/creeper.webp")
       HTML_Body.appendChild(creeper)
       creeper.addEventListener("mouseover", function () {
         creeper.classList.add("creeper_fade")
