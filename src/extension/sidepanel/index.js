437de5bf33798d058d702d7c4b26efd4763248c0
atomu21263 <aatomu21263+git@gmail.com>
2025-10-08 22:31:20 +0900

    🎨 classメゾットできれいに

--- src/extension/sidepanel/index.js
+++ src/extension/sidepanel/index.js
@@ -1,16 +1,18 @@
+// MARK: Const
 const SlotLever = document.getElementById("slot-lever")
 const SlotLeverButton = document.getElementById("slot-lever-button")
 const SlotReel = document.getElementById("slot-reel")
 const InfoSymbol = document.getElementById("info-symbol")
 
+// MARK: Vars
 let isSpinning
 
 
-
+// MARK: ReelSymbol
 class ReelSymbol {
   /** @type {Map<string, SlotSymbol>} */
   symbols = new Map()
-  /** @type {{total:number,list:{weight:number,symbol:string}[]}} */
+  /** @type {{total:number,list:{value:number,symbol:string}[]}} */
   weights = { total: 0, list: [] }
 
   constructor() { }
@@ -34,11 +36,46 @@ class ReelSymbol {
     })
   }
 
+  Update() {
+    while (InfoSymbol.children.length > 0) {
+      InfoSymbol.children[0].remove()
+    }
+
+    this.weights.total = 0
+    this.weights.list = []
+    for (const key of this.symbols.keys()) {
+      const symbol = this.symbols.get(key)
+      this.weights.total += symbol.weight
+      this.weights.list.push({ value: this.weights.total, symbol: key })
+    }
+
+    for (const weight of this.weights.list) {
+      const symbol = this.symbols.get(weight.symbol)
+      const tr = document.createElement("tr")
+
+      const nameBody = document.createElement("td")
+      const name = document.createElement("img")
+      name.src = symbol.url
+      nameBody.append(name)
+      tr.append(nameBody)
+
+      const chance = document.createElement("td")
+      chance.textContent = ((symbol.weight / this.weights.total) * 100).toFixed(2) + "%"
+      tr.append(chance)
+
+      const value = document.createElement("td")
+      value.textContent = `${symbol.value.current}(x${symbol.value.amplifier})`
+      tr.append(value)
+
+      InfoSymbol.append(tr)
+    }
+  }
+
   GetRandom() {
     const rand = Math.floor(Math.random() * this.weights.total);
 
     for (let i = 0; i < this.weights.list.length; i++) {
-      if (rand < this.weights.list[i].weight) return this.weights.list[i].symbol
+      if (rand < this.weights.list[i].value) return this.weights.list[i].symbol
     }
     return this.weights.list[0].symbol
   }
@@ -61,6 +98,7 @@ Symbols.Add("E", "./assets/gold.png", 3, 7)
 Symbols.Add("F", "./assets/emerald.png", 1, 10)
 Symbols.Add("G", "./assets/diamond.png", 1, 10)
 
+// MARK: ReelData
 class Reel {
   // hidden x3, view x3
   reel = [
@@ -71,7 +109,7 @@ class Reel {
     ["", "", "", "", "", ""],
   ]
   constructor() {
-    UpdateSymbol()
+    Symbols.Update()
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 6; j++) {
         this.reel[i][j] = Symbols.GetRandom()
@@ -125,6 +163,7 @@ class Reel {
 const ReelData = new Reel()
 ReelData.Place()
 
+// MARK: Slot Lever
 SlotLever.addEventListener("click", async () => {
   if (isSpinning) return
   isSpinning = true
@@ -135,40 +174,5 @@ SlotLever.addEventListener("click", async () => {
   SlotLeverButton.classList.remove("pull")
   isSpinning = false
 })
-
-function UpdateSymbol() {
-  while (InfoSymbol.children.length > 0) {
-    InfoSymbol.children[0].remove()
-  }
-
-  WeightedSymbol.total = 0
-  WeightedSymbol.weights = []
-  for (const key of Symbols.keys()) {
-    const symbol = Symbols.get(key)
-    WeightedSymbol.total += symbol.weight
-    WeightedSymbol.weights.push({ weight: WeightedSymbol.total, symbol: key })
-  }
-
-  for (const symbolInfo of WeightedSymbol.weights) {
-    const symbol = Symbols.get(symbolInfo.symbol)
-    const tr = document.createElement("tr")
-
-    const nameBody = document.createElement("td")
-    const name = document.createElement("img")
-    name.src = symbol.url
-    nameBody.append(name)
-    tr.append(nameBody)
-
-    const chance = document.createElement("td")
-    chance.textContent = ((symbol.weight / WeightedSymbol.total) * 100).toFixed(2) + "%"
-    tr.append(chance)
-
-    const value = document.createElement("td")
-    value.textContent = `${symbol.value.current}(x${symbol.value.amplifier})`
-    tr.append(value)
-
-    InfoSymbol.append(tr)
-  }
-}
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
